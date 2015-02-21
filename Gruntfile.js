19e6efdc5e99482cd6fd068c5bfc05984050b71f
Jonathan Belcher <jonathan_belcher@cable.comcast.com>
2015-02-21 14:00:36 -0500

    Removed cruft and updated build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -196,8 +196,8 @@ module.exports = function (grunt) {
           strip: true
         },
         files: {
-          '<%= yeoman.dist %>/elements/elements.vulcanized.html': [
-            '<%= yeoman.dist %>/elements/elements.html'
+          '<%= yeoman.dist %>/elements/where-app/where-app.vulcanized.html': [
+            '<%= yeoman.dist %>/elements/where-app/where-app.html'
           ]
         }
       }
